308656edf797745f854220d16a15b45e9db68dfc
Konstantinos Lypitkas <klipitkas@gmail.com>
2021-11-11 12:12:45 +0200

    Add more spacing in the sidebar

--- src/AppMenu.js
+++ src/AppMenu.js
@@ -7,21 +7,16 @@ const AppMenu = ({ onMenuClick }) => {
   const { t } = useTranslation();
 
   return (
-    <div
-      className="layout-sidebar"
-      role="button"
-      tabIndex="0"
-      onClick={onMenuClick}
-    >
+    <div className="layout-sidebar" role="button" tabIndex="0" onClick={onMenuClick}>
       <div className="logo">
         <NavLink to="/">
           <div className="p-d-flex p-ai-center">
             <img
               id="app-logo"
-              className="logo-image"
+              className="logo-image p-mr-3"
               src={Logo}
               alt="LUP4LDN"
-              style={{ height: '50px', width: '50px', marginRight: '15px' }}
+              style={{ height: '50px', width: '50px' }}
             />
             <h3 style={{ color: 'white', textAlign: 'left' }}>
               LUP4LDN
@@ -35,18 +30,16 @@ const AppMenu = ({ onMenuClick }) => {
         <ul className="layout-menu" role="menu">
           <li className="layout-root-menuitem" role="menuitem">
             <div className="layout-root-menuitem">
-              <div className="layout-menuitem-root-text" style={{ textTransform: 'uppercase' }}>
-                {t('MY_PROJECTS')}
-              </div>
+              <div className="layout-menuitem-root-text">{t('MY_PROJECTS')}</div>
             </div>
             <ul className="layout-menu" role="menu">
-              <li className="" role="menuitem">
+              <li className="p-mb-1" role="menuitem">
                 <NavLink to="/create-project" activeClassName="p-button" exact>
                   <i className="layout-menuitem-icon fad fa-file-plus" />
-                  <span className="layout-menuitem-text">{t('CREATE_NEW')}</span>
+                  <span className="layout-menuitem-text">{t('NEW_PROJECT')}</span>
                 </NavLink>
               </li>
-              <li className="" role="menuitem">
+              <li className="p-mb-1" role="menuitem">
                 <NavLink to="/" activeClassName="p-button" exact>
                   <i className="layout-menuitem-icon fad fa-columns" />
                   <span className="layout-menuitem-text">{t('DASHBOARD')}</span>

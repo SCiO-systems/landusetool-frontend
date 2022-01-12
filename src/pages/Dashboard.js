75c3d0e542befc0dedf1a504b858cad5745187b7
Konstantinos Lypitkas <klipitkas@gmail.com>
2022-01-12 11:08:06 +0200

    Fix minor dashboard button margin issue

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import InviteProjectMembersDialog from '../components/dialogs/InviteProjectMembersDialog';
 import { listProjects, PROJECT_OWNER } from '../services/projects';
@@ -41,7 +41,7 @@ const Dashboard = () => {
     <div className="layout-dashboard">
       <div className="p-grid">
         {projects.map(({ id, title, acronym, description, role }) => (
-          <div key={id} className="p-col-4">
+          <div key={id} className="p-sm-6 p-md-6 p-lg-4">
             <Card
               title={title}
               subTitle={acronym}
@@ -49,16 +49,16 @@ const Dashboard = () => {
                 <>
                   <Button
                     onClick={() => goToProject(id)}
-                    label="Edit Project"
+                    label="Edit"
                     icon="pi pi-cog"
-                    className="p-mr-2"
+                    className="p-mr-2 p-mt-2"
                   />
                   {role === PROJECT_OWNER && (
                     <Button
                       onClick={() => inviteToProject(id)}
                       label="Invite Members"
                       icon="pi pi-user-plus"
-                      className="p-mr-2 p-button-secondary"
+                      className="p-mr-2 p-mt-2 p-button-secondary"
                     />
                   )}
                 </>

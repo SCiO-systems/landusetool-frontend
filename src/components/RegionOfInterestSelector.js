a8ddd88e78e945f4b1825cb1596287fe96fe53a9
Nasos Lamprokostopoulos <nlamprok@gmail.com>
2022-01-20 11:18:42 +0200

    Move LU classification scheme to a separate card on create project

--- src/components/RegionOfInterestSelector.js
+++ src/components/RegionOfInterestSelector.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from 'primereact/button';
 import { Steps } from 'primereact/steps';
-import { RadioButton } from 'primereact/radiobutton';
 import { useTranslation } from 'react-i18next';
 
-import MultipleKeyValueEntriesTable from './MultipleKeyValueEntriesTable';
 import Glowglobe from './glowglobe';
 import CountrySelector from './CountrySelector';
 import { getCountryAdminLevelArea, getByCoordinates } from '../services/polygons';
@@ -15,14 +13,10 @@ const RegionOfInterestSelector = ({ register, setValue }) => {
   const [country, setCountry] = useState(undefined);
   const [adminLevel, setAdminLevel] = useState(1);
   const [layers, setLayers] = useState([]);
-  const [luClasses, setLuClasses] = useState([]);
-  const [useDefaultLuClasses, setUseDefaultLuClasses] = useState(true);
 
   register('country', { required: true });
   register('adminLevel', { required: true });
   register('polygon', { required: true });
-  register('defaultLuClasses', { value: true });
-  register('luClasses', { required: false });
 
   useEffect(() => {
     setValue('country', country);
@@ -40,10 +34,6 @@ const RegionOfInterestSelector = ({ register, setValue }) => {
         });
     }
   }, [country, adminLevel, setValue]);
-
-  useEffect(() => {
-    setValue('luClasses', JSON.stringify(luClasses));
-  }, [luClasses, setValue]);
 
   const glowglobeOptions = {
     mode: 'select_administration_area',
@@ -83,13 +73,6 @@ const RegionOfInterestSelector = ({ register, setValue }) => {
         setActiveIndex(1);
       },
     },
-    {
-      label: t('SETUP_LU_CLASSES'),
-      command: (_e) => {
-        if (country === undefined) return;
-        setActiveIndex(2);
-      },
-    },
   ];
 
   return (
@@ -111,55 +94,6 @@ const RegionOfInterestSelector = ({ register, setValue }) => {
           setAdminLevel={setAdminLevel}
         />
       )}
-      {activeIndex === 2 && (
-        <>
-          <div className="p-field-radiobutton">
-            <RadioButton
-              inputId="defaultLuClasses"
-              value="true"
-              name="defaultLuClasses"
-              onChange={() => {
-                setUseDefaultLuClasses(true);
-                setValue('defaultLuClasses', true);
-              }}
-              checked={useDefaultLuClasses}
-            />
-            <label htmlFor="defaultLuClasses">{t('USE_DEFAULT_LU_CLASSIFICATION')}</label>
-          </div>
-          <div className="p-field-radiobutton">
-            <RadioButton
-              inputId="defaultLuClasses"
-              value="false"
-              name="defaultLuClasses"
-              onChange={() => {
-                setUseDefaultLuClasses(false);
-                setValue('defaultLuClasses', false);
-              }}
-              checked={!useDefaultLuClasses}
-            />
-            <label htmlFor="defaultLuClasses">{t('USE_CUSTOM_LU_CLASSIFICATION')}</label>
-          </div>
-          {!useDefaultLuClasses && (
-            <MultipleKeyValueEntriesTable
-              className="p-mt-4"
-              keyLabel={t('NAME')}
-              valueLabel={t('VALUE')}
-              header={t('CUSTOM_LU_CLASSES')}
-              data={luClasses}
-              onAddItem={(entry) =>
-                setLuClasses(
-                  [...luClasses, entry]
-                )
-              }
-              onDeleteItem={(entry) => {
-                setLuClasses(
-                  luClasses.filter((lc) => lc.key !== entry.key)
-                );
-              }}
-            />
-          )}
-        </>
-      )}
       <div className="p-d-flex p-jc-between p-mt-6 p-mb-2">
         <Button
           className="p-button-secondary"
@@ -172,7 +106,7 @@ const RegionOfInterestSelector = ({ register, setValue }) => {
         <Button
           className="p-button-secondary"
           type="button"
-          disabled={activeIndex === 2 || country === undefined}
+          disabled={activeIndex === 1 || country === undefined}
           label={t('NEXT')}
           onClick={(_e) => setActiveIndex((oldIndex) => (oldIndex + 1))}
           icon="pi pi-angle-right"
--- src/pages/CreateProject.js
+++ src/pages/CreateProject.js
@@ -4,8 +4,11 @@ import { useForm } from 'react-hook-form';
 import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
 import { useHistory } from 'react-router-dom';
+import { RadioButton } from 'primereact/radiobutton';
+
 import ProjectDetails from '../components/forms/ProjectDetails';
 import RegionOfInterestSelector from '../components/RegionOfInterestSelector';
+import MultipleKeyValueEntriesTable from '../components/MultipleKeyValueEntriesTable';
 import { createProject } from '../services/projects';
 import { ToastContext } from '../store';
 import { handleError } from '../utilities/errors';
@@ -17,6 +20,8 @@ const CreateProject = () => {
   const { setError, setSuccess } = useContext(ToastContext);
   const [readyToSubmit, setReadyToSubmit] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [luClasses, setLuClasses] = useState([]);
+  const [useDefaultLuClasses, setUseDefaultLuClasses] = useState(true);
 
   const onSubmit = async (data) => {
     try {
@@ -30,6 +35,13 @@ const CreateProject = () => {
       setLoading(false);
     }
   };
+
+  register('defaultLuClasses', { value: true });
+  register('luClasses', { required: false });
+
+  useEffect(() => {
+    setValue('luClasses', JSON.stringify(luClasses));
+  }, [luClasses, setValue]);
 
   useEffect(() => {
     const subscription = watch((value) => {
@@ -48,6 +60,53 @@ const CreateProject = () => {
         </Card>
         <Card className="p-mt-4" title={t('REGION_OF_INTEREST')}>
           <RegionOfInterestSelector register={register} setValue={setValue} />
+        </Card>
+        <Card className="p-mt-4" title={t('LAND_USE')}>
+          <div className="p-field-radiobutton">
+            <RadioButton
+              inputId="defaultLuClasses"
+              value="true"
+              name="defaultLuClasses"
+              onChange={() => {
+                setUseDefaultLuClasses(true);
+                setValue('defaultLuClasses', true);
+              }}
+              checked={useDefaultLuClasses}
+            />
+            <label htmlFor="defaultLuClasses">{t('USE_DEFAULT_LU_CLASSIFICATION')}</label>
+          </div>
+          <div className="p-field-radiobutton">
+            <RadioButton
+              inputId="defaultLuClasses"
+              value="false"
+              name="defaultLuClasses"
+              onChange={() => {
+                setUseDefaultLuClasses(false);
+                setValue('defaultLuClasses', false);
+              }}
+              checked={!useDefaultLuClasses}
+            />
+            <label htmlFor="defaultLuClasses">{t('USE_CUSTOM_LU_CLASSIFICATION')}</label>
+          </div>
+          {!useDefaultLuClasses && (
+            <MultipleKeyValueEntriesTable
+              className="p-mt-4"
+              keyLabel={t('NAME')}
+              valueLabel={t('VALUE')}
+              header={t('CUSTOM_LU_CLASSES')}
+              data={luClasses}
+              onAddItem={(entry) =>
+                setLuClasses(
+                  [...luClasses, entry]
+                )
+              }
+              onDeleteItem={(entry) => {
+                setLuClasses(
+                  luClasses.filter((lc) => lc.key !== entry.key)
+                );
+              }}
+            />
+          )}
         </Card>
         <div className="p-d-flex p-jc-start p-mt-4 p-mb-6">
           <Button className="p-button-lg" loading={loading} disabled={!readyToSubmit || loading} type="submit" label={t('CREATE_PROJECT')} icon="pi pi-plus" />
